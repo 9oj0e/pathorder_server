566e1eaa365b16a314914dbc3760067f4660ab76
9oj0e <war2290@gmail.com>
2024-04-24 15:58:41 +0900

    feat: core entities(subject) init

--- src/main/java/shop/project/pathorderserver/store/Store.java
+++ src/main/java/shop/project/pathorderserver/store/Store.java
@@ -3,6 +3,8 @@ package shop.project.pathorderserver.store;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 @Entity
 @Table(name = "store_tb")
@@ -10,4 +12,25 @@ public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    // 회원 정보
+    private String username;
+    private String password;
+    private boolean status; // 계정 상태 (0 : 비활성, 1 : 활성)
+    private Timestamp registeredAt; // 가입일
+    // 사업자 정보
+    private String ownerName; // 사업자 이름
+    private String ownerTel;
+    private String ownerEmail;
+    private String bizNum; // 사업자 등록 번호
+    // 매장 정보
+    private String name;
+    private String tel; // 매장 번호
+    private String intro; // 매장 소개
+    private String imgSrc;
+    private Timestamp openingTime;
+    private Timestamp closingTime;
+    private String closedDay; // 휴무일
+    private String address; // 주소
+    private double latitude; // 주소 좌표 위도
+    private double longitude; // 주소 좌표 경도
 }
--- src/main/java/shop/project/pathorderserver/user/User.java
+++ src/main/java/shop/project/pathorderserver/user/User.java
@@ -3,6 +3,8 @@ package shop.project.pathorderserver.user;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 @Entity
 @Table(name = "user_tb")
@@ -10,4 +12,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    // 회원 정보
+    private String username;
+    private String password;
+    private String nickname; // 회원 별명
+    private boolean status; // 계정 상태 (0 : 비활성, 1 : 활성)
+    private Timestamp registered_at; // 가입일
+    // 개인 정보
+    private String name;
+    private String tel; // 전화번호
+    private String email;
+    private String imgSrc;
 }
