93a3a9610145d572749b3004aab7f94e9f6af30b
9oj0e <war2290@gmail.com>
2024-04-25 16:46:29 +0900

    docs: cont,dto,serv

--- src/main/java/shop/project/pathorderserver/user/UserController.java
+++ src/main/java/shop/project/pathorderserver/user/UserController.java
@@ -2,6 +2,9 @@ package shop.project.pathorderserver.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import shop.project.pathorderserver._core.utils.ApiUtil;
 
@@ -10,15 +13,21 @@ import shop.project.pathorderserver._core.utils.ApiUtil;
 public class UserController {
     private final UserService userService;
 
-    public ResponseEntity<?> join() { // 회원가입
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO requestDTO) { // 회원가입
+
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
 
+    @PostMapping("/login")
     public ResponseEntity<?> login() { // 로그인
+
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
 
+    @GetMapping("/logout")
     public ResponseEntity<?> logout() { // 로그아웃
+
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
 }
