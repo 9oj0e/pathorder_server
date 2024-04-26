7fb76d4535afa8fbc61a3aebca9f50533d093790
9oj0e <war2290@gmail.com>
2024-04-26 17:56:33 +0900

    feat: controller bound to service

--- src/main/java/shop/project/pathorderserver/user/UserService.java
+++ src/main/java/shop/project/pathorderserver/user/UserService.java
@@ -10,25 +10,25 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional // 회원 가입
-    public UserResponse.JoinDTO createUser(UserRequest.JoinDTO requestDTO) {
+    public UserResponse.JoinDTO createUser(UserRequest.JoinDTO reqDTO) {
 
         return null;
     }
 
     // 로그인
-    public UserResponse.LoginDTO getUser(UserRequest.LoginDTO requestDTO) {
+    public UserResponse.LoginDTO getUser(UserRequest.LoginDTO reqDTO) {
 
         return null;
     }
 
     // 회원정보 보기
-    public UserResponse.UserDTO getUser() {
+    public UserResponse.UserDTO getUser(int userId) {
 
         return null;
     }
 
     @Transactional // 회원정보 수정
-    public UserResponse.UpdateDTO setUser(UserRequest.UpdateDTO requestDTO) {
+    public UserResponse.UpdateDTO setUser(UserRequest.UpdateDTO reqDTO) {
 
         return null;
     }
