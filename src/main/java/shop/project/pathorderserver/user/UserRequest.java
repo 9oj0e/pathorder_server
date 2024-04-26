300372d5489728e4e236b4c281bfd0659369a3d9
9oj0e <war2290@gmail.com>
2024-04-26 23:30:02 +0900

    feat: userRequestDTO

--- src/main/java/shop/project/pathorderserver/user/UserRequest.java
+++ src/main/java/shop/project/pathorderserver/user/UserRequest.java
@@ -4,19 +4,33 @@ import lombok.Data;
 
 public class UserRequest {
 
-    @Data
+    @Data // 회원 가입
     public static class JoinDTO {
-
+        // 회원 정보
+        private String username;
+        private String password;
+        private String nickname; // 회원 별명
+        // 개인 정보
+        private String name;
+        private String tel;
+        private String email;
     }
 
-    @Data
+    @Data // 로그인
     public static class LoginDTO {
+        private String username;
+        private String password; // TODO: 암호화
 
     }
 
-    @Data
+    @Data // 회원 정보 수정
     public static class UpdateDTO {
-
+        // 회원 정보
+        // private String password; // view 기준
+        private String nickname;
+        // 개인 정보
+        private String tel;
+        private String email;
     }
 
     @Data
