bff48f8b9464a7e9674d19767b2bda4abe075d90
Hyeonjeong-JANG <139729358+Hyeonjeong-JANG@users.noreply.github.com>
2024-04-26 22:55:11 +0900

    feat: 매장 목록보기 (fix: Store 엔티티 수정) (#10)
    * feat(fix): 매장 목록 보기(Store 엔티티 수정)
    
    * fix: 엔티티 수정(wrapper class)
    
    * chore: 테스트
    
    * chore: 메서드 람다로 변경
    
    * chore: 서비스 정렬 삭제
    
    * Update StoreController.java
    
    * Update StoreResponse.java
    
    unused imports removed
    
    * Update StoreService.java
    
    unused imports removed
    
    ---------
    
    Co-authored-by: David Jo <126438339+9oj0e@users.noreply.github.com>

--- src/main/java/shop/project/pathorderserver/store/StoreResponse.java
+++ src/main/java/shop/project/pathorderserver/store/StoreResponse.java
@@ -1,4 +1,29 @@
 package shop.project.pathorderserver.store;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StoreResponse {
+
+    @Data
+    public static class ListingsDTO {
+        private int id;
+        private String imgSrc;
+        private String name;
+        // TODO: 주소 좌표로 거리를 계산할 수 있을 것인가? latitude, longitude 속성 활용? (지도 api가 알아서 해주길 바람..)
+        private int distance;
+        private int likeCount;
+
+        public ListingsDTO(Store store) {
+            this.id = store.getId();
+            this.imgSrc = store.getImgSrc();
+            this.name = store.getName();
+            this.distance = 163;
+            this.likeCount = 181;
+        }
+    }
 }
--- src/main/java/shop/project/pathorderserver/store/StoreService.java
+++ src/main/java/shop/project/pathorderserver/store/StoreService.java
@@ -2,9 +2,19 @@ package shop.project.pathorderserver.store;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class StoreService {
     private final StoreRepository storeRepository;
+
+    public List<StoreResponse.ListingsDTO> getStoreList() {
+        List<Store> stores = storeRepository.findAll();
+        
+        return stores.stream().map(StoreResponse.ListingsDTO::new).toList();
+    }
 }
