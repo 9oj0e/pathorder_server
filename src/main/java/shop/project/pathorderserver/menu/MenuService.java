850f5c826a9513f1f54f32bc1b3c40d611e06cee
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-04-26 22:18:25 +0900

    feat: 메뉴 목록보기 (#9)
    * menuList 만드는 중
    
    * feat: 매장별 메뉴 리스트
    
    * Update MenuResponse.java
    
    menuName -> name
    
    Menu 페이지에 응답할 name은
    menu의 name이 될 것.
    
    store.name을 가져와야 한다면, 그 때 고민하는 걸로.
    
    ---------
    
    Co-authored-by: David Jo <126438339+9oj0e@users.noreply.github.com>

--- src/main/java/shop/project/pathorderserver/menu/MenuService.java
+++ src/main/java/shop/project/pathorderserver/menu/MenuService.java
@@ -3,8 +3,16 @@ package shop.project.pathorderserver.menu;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class MenuService {
     private final MenuRepository menuRepository;
+
+    // 매장별 메뉴 리스트
+    public List<MenuResponse.StoreMenuDTO> getAllStoreMenu(int storeId) {
+        List<Menu> menuList = menuRepository.findAllMenuByStoreId(storeId).get(); // TODO: orElseThrow
+        return menuList.stream().map(menu -> new MenuResponse.StoreMenuDTO(menu)).toList();
+    }
 }
--- src/main/java/shop/project/pathorderserver/order/OrderController.java
+++ src/main/java/shop/project/pathorderserver/order/OrderController.java
@@ -1,10 +1,29 @@
 package shop.project.pathorderserver.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import shop.project.pathorderserver._core.utils.ApiUtil;
+import shop.project.pathorderserver.menu.Menu;
+import shop.project.pathorderserver.menu.MenuResponse;
+import shop.project.pathorderserver.menu.MenuService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class OrderController {
     private final OrderService orderService;
+    private final MenuService menuService;
+
+    // 매장별 메뉴 리스트
+    @GetMapping("/api/stores/{storeId}/menus")
+    private ResponseEntity<?> menuList(@PathVariable int storeId) {
+        List<MenuResponse.StoreMenuDTO> menuList = menuService.getAllStoreMenu(storeId);
+
+        return ResponseEntity.ok(new ApiUtil(menuList));
+    }
 }
