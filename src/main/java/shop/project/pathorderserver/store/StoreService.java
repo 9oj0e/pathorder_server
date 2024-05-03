8e0efa9513e04e0094e64c5921c1484539423557
kimsjae <stwin755@gmail.com>
2024-05-03 15:26:54 +0900

    refactor: 서비스에서 업데이트하는 로직을 엔티티에 메서드로 이동

--- src/main/java/shop/project/pathorderserver/store/StoreService.java
+++ src/main/java/shop/project/pathorderserver/store/StoreService.java
@@ -107,20 +107,7 @@ public class StoreService {
         Store store = storeRepository.findById(sessionStoreId)
                 .orElseThrow(() -> new Exception403("수정할 권한이 없습니다."));
 
-        store.setUsername(reqDTO.getUsername());
-        store.setPassword(reqDTO.getPassword());
-        store.setOwnerName(reqDTO.getOwnerName());
-        store.setOwnerTel(reqDTO.getOwnerTel());
-        store.setOwnerEmail(reqDTO.getOwnerEmail());
-        store.setBizNum(reqDTO.getBizNum());
-        store.setImgFilename(reqDTO.getImgFilename());
-        store.setName(reqDTO.getName());
-        store.setTel(reqDTO.getTel());
-        store.setIntro(reqDTO.getIntro());
-        store.setOpeningTime(reqDTO.getOpeningTime());
-        store.setClosingTime(reqDTO.getClosingTime());
-        store.setClosedDay(reqDTO.getClosedDay());
-        store.setAddress(reqDTO.getAddress());
+        store.update(reqDTO);
 
         SessionStore sessionStore = new SessionStore(store);
 
