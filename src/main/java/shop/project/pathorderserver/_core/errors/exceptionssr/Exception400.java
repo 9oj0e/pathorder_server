247fd18b4e7bd3a6fc56f88a561fc9dc36c88971
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-05-09 11:42:15 +0900

    feat: Web 서버 예외처리 (fix: 매장 수정하기 폼 매핑) (#54)
    * fix: /1 -> sessionStore.id 변경
    
    * fix: 매장 수정하기 - 데이터 받아오기
    
    * error 진행중
    
    * fix: view응답이 안 되는 예외처리 해결

---
+++ src/main/java/shop/project/pathorderserver/_core/errors/exceptionssr/Exception400.java
@@ -0,0 +1,8 @@
+package shop.project.pathorderserver._core.errors.exceptionssr;
+
+public class Exception400 extends RuntimeException {
+
+    public Exception400(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/shop/project/pathorderserver/_core/errors/exceptionssr/Exception403.java
@@ -0,0 +1,8 @@
+package shop.project.pathorderserver._core.errors.exceptionssr;
+
+public class Exception403 extends RuntimeException {
+
+    public Exception403(String msg) {
+        super(msg);
+    }
+}
