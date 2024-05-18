ecd3dd2308d3619bb620931f1825cd2290c2ad65
9oj0e <war2290@gmail.com>
2024-05-18 18:09:54 +0900

    temp: store sse package

--- src/main/java/shop/project/pathorderserver/store/StoreSseController.java
+++ src/main/java/shop/project/pathorderserver/store/StoreSseController.java
@@ -1,20 +1,22 @@
 package shop.project.pathorderserver.store;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import shop.project.pathorderserver._core.utils.ApiUtil;
 
+@RequiredArgsConstructor
 @RestController
-public class StoreSSEController {
+public class StoreSseController {
+    private final StoreSseService storeSSEService;
+    private final HttpSession session;
 
-    @Scheduled(fixedRate = 1000)
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter connect() {
+        SessionStore sessionStore = (SessionStore) session.getAttribute("sessionStore");
 
-        return new SseEmitter();
+        return storeSSEService.createConnection(sessionStore.getId());
     }
 }
---
+++ src/main/java/shop/project/pathorderserver/store/StoreSseService.java
@@ -0,0 +1,48 @@
+package shop.project.pathorderserver.store;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import shop.project.pathorderserver._core.errors.exceptionssr.Exception400;
+
+@RequiredArgsConstructor
+@Service
+public class StoreSseService {
+    private final StoreSseRepository storeSSERepository;
+    private static final long TIMEOUT = 60 * 1000;
+    private static final long RECONNECTION_TIMEOUT = 1000L;
+    // OnCompletion: Emitter 가 완료될 때(모든 데이터가 성공적으로 전송된 상태)
+    // OnTimeout: Emitter 가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때)
+
+    public SseEmitter createConnection(int storeId) {
+        SseEmitter emitter = new SseEmitter(TIMEOUT);
+        storeSSERepository.save(storeId, emitter);
+        emitter.onTimeout(() -> storeSSERepository.deleteById(storeId));
+        createEvent(storeId, "EventStream 생성");
+        emitter.onCompletion(() -> storeSSERepository.deleteById(storeId));
+        return emitter;
+    }
+
+    public void createOrderNotification(int orderId, int storeId) {
+        SseEmitter emitter = storeSSERepository.findById(storeId)
+                .orElseThrow(() -> new Exception400("서버가 비활성화 상태입니다."));
+        emitter.onTimeout(() -> storeSSERepository.deleteById(storeId));
+        createEvent(storeId, orderId + "번 주문이 생성되었습니다.");
+        emitter.onCompletion(() -> storeSSERepository.deleteById(storeId));
+    }
+
+    public void createEvent(int storeId, String data) {
+        SseEmitter emitter = storeSSERepository.findById(storeId)
+                .orElseThrow(() -> new Exception400("서버가 비활성화 상태입니다."));
+        SseEmitter.SseEventBuilder event = SseEmitter.event()
+                .name("sse")
+                .data(data)
+                .reconnectTime(RECONNECTION_TIMEOUT);
+        try {
+            emitter.send(event);
+        } catch (Exception e) {
+            storeSSERepository.deleteById(storeId);
+            emitter.completeWithError(e);
+        }
+    }
+}
