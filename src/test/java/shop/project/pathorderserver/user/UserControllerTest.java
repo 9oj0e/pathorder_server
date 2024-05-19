e67313045c9053644c788a85d48d8207a6980f78
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-05-19 16:57:36 +0900

    test: join_username_same_fail_test

--- src/test/java/shop/project/pathorderserver/user/UserControllerTest.java
+++ src/test/java/shop/project/pathorderserver/user/UserControllerTest.java
@@ -37,6 +37,7 @@ public class UserControllerTest {
                         .build());
     }
 
+    // 회원가입 성공
     @Test
     public void join_test() throws Exception {
         //given
@@ -59,11 +60,39 @@ public class UserControllerTest {
         System.out.println("respBody = " + respBody);
 
         // then
-actions.andExpect(status().isOk());
-actions.andExpect(jsonPath("$.status").value(200));
-actions.andExpect(jsonPath("$.msg").value("성공"));
-actions.andExpect(jsonPath("$.body.username").value("user6"));
-actions.andExpect(jsonPath("$.body.nickname").value("지영"));
+        actions.andExpect(status().isOk());
+        actions.andExpect(jsonPath("$.status").value(200));
+        actions.andExpect(jsonPath("$.msg").value("성공"));
+        actions.andExpect(jsonPath("$.body.username").value("user6"));
+        actions.andExpect(jsonPath("$.body.nickname").value("지영"));
+    }
 
+    // 회원가입 실패(중복되는 유저네임 존재)
+    @Test
+    public void join_username_same_fail_test() throws Exception {
+        //given
+        UserRequest.JoinDTO reqDTO = new UserRequest.JoinDTO();
+        reqDTO.setUsername("user1");
+        reqDTO.setPassword("1234");
+        reqDTO.setNickname("성재");
+
+        String reqBody = om.writeValueAsString(reqDTO);
+
+        // when
+        ResultActions actions = mvc.perform(
+                post("/join")
+                        .content(reqBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // eye
+        String respBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println("respBody = " + respBody);
+
+        // then
+        actions.andExpect(status().isBadRequest());
+        actions.andExpect(jsonPath("$.status").value(400));
+        actions.andExpect(jsonPath("$.msg").value("중복된 유저입니다."));
+        actions.andExpect(jsonPath("$.body").isEmpty());
     }
 }
